eaaf8c5f356a5992b377ad430b203ef377d4c1b5
sean@dague.net <sean@dague.net>
2008-05-03 08:17:26 -0400

    added inviter

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '0ff2bb41e41c19ef9afe02dc6f4fc3c9'
+end
