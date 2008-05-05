a071d07e7398704f7c4287555f321c48f8d159ea
sean@dague.net <sean@dague.net>
2008-05-04 22:27:05 -0400

    fix up the sorry controller

--- app/controllers/sorry_controller.rb
+++ app/controllers/sorry_controller.rb
@@ -1,5 +1,15 @@
 class SorryController < ApplicationController
+    around_filter :catch_exceptions
+    
+    private
+    def catch_exceptions
+        yield
+    rescue => exception
+        redirect_to(:controller => "sorry")
+    end
+    
+    public 
+    def index
+    end
 
-  def index
-  end
 end
