de9df6f9e442c3055cad2a6f3a0b24810cba1db8
sean@dague.net <sean@dague.net>
2008-05-08 08:57:45 -0400

    change sort order

--- app/helpers/invite_helper.rb
+++ app/helpers/invite_helper.rb
@@ -38,7 +38,7 @@ module InviteHelper
     end
     
     def number_by_state(state)
-        rsvps = Rsvp.find(:all, :conditions => ["state = ?", state]).sort_by {|s| s.person.email}
+        rsvps = Rsvp.find(:all, :conditions => ["state = ?", state])
         count = 0
         rsvps.each do |r|
             count += (r.num or 1)
@@ -47,7 +47,7 @@ module InviteHelper
     end
     
     def list_by_state(state)
-        Rsvp.find(:all, :conditions => ["state = ?", state]).sort_by {|s| s.person.email}
+        Rsvp.find(:all, :conditions => ["state = ?", state]).sort_by {|s| s.updated_at.to_s + s.person.email }
     end 
 
     def format_invitee_admin(r)
